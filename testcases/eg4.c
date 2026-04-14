fe6db5e52735e0cd6f46a33dfbe4a979cc22d981
kanishk <itskanishk36@gmail.com>
2026-04-14 18:22:29 +0530

    Assignment - Input Box

--- testcases/eg4.c
+++ testcases/eg4.c
@@ -3,6 +3,36 @@
 #include<string.h>
 #include<termios.h>
 #include<unistd.h>
+
+// for tesing 
+void print_cursor_index(int k)
+{
+    char a[11];
+    sprintf(a,"%d",k);
+    kui_say(20,2,a);
+}
+// for tesing 
+void print_size(int i)
+{
+    char a[11];
+    sprintf(a,"%d",i);
+    kui_say(20,6,a);
+}
+// for tesing 
+void print_cursor_row(int cursor_row)
+{
+    char a[11];
+    sprintf(a,"%d",cursor_row);
+    kui_say(20,10,a);
+}
+// for tesing 
+void print_cursor_column(int cursor_column)
+{
+    char a[11];
+    sprintf(a,"%d",cursor_column);
+    kui_say(20,14,a);
+}
+
 void kui_input_box(int row,int column,char *array,int size,int screen_width,char ld,char rd)
 {
     struct termios new_t,old_t;
@@ -14,7 +44,6 @@ void kui_input_box(int row,int column,char *array,int size,int screen_width,char
     int k;
     int z;
     int cursor_row,cursor_column;
-    int cursorMoved=0;
     if(row<1 || column<1) return;
     if(array==NULL) return;
     if(size<1) return;
@@ -40,8 +69,6 @@ void kui_input_box(int row,int column,char *array,int size,int screen_width,char
     i=0;
     while(1)
     {
-        if(cursorMoved==0)
-        {
             r=row;
             c=column;
             kui_clear();
@@ -62,41 +89,43 @@ void kui_input_box(int row,int column,char *array,int size,int screen_width,char
                     kui_go_to_xy(r,c+1);
                  }
             }
-            if(i!=k)
-            {
-                kui_go_to_xy(cursor_row,cursor_column);
-            }
-            else
-            {
-                cursor_row=r;
-                cursor_column=column + (k%screen_width)+1;
-            }
+        
+        write(fileno(stdout),"\033[s",3); // for testing
+        if(i!=k)
+        {
+            cursor_row=row+(k/screen_width);
+            cursor_column=column+(k%screen_width)+1;
+            kui_go_to_xy(cursor_row,cursor_column);
+            write(fileno(stdout),"\033[s",3);
         }
+        else
+        {
+            cursor_row=row+(i/screen_width);
+            cursor_column=column+(i%screen_width)+1;
+        }
+        print_cursor_index(k);                  // for testing
+        print_size(i);                          // for testing
+        print_cursor_row(cursor_row);           // for testing
+        print_cursor_column(cursor_column);     // for testing
+
+        write(fileno(stdout),"\033[u",3);   // for testing
+
         m=getchar();
         fflush(stdin);
-        if(i+1==size) break;
+        if(i==size) break;
         if(m==10) break;
-        if(m==127 && i>0)
+        if(m==127)
         {
-            if(k==0)
-            {
-                cursor_column=column+1;
-            }
-            else
+            if(k>0)
             {
             // write(fileno(stdout),"\033[1D",3);
-            for(z=k-1;z<i;z++)
-            {
-                array[z]=array[z+1];
-            }
-            i--;
-            array[i]='\0';
-            k--;
-
-            cursor_row=row + (k/screen_width);  
-            cursor_column=column + (k%screen_width) + 1;
-            cursorMoved=0;
-            continue;
+                for(z=k-1;z<i;z++)
+                {
+                    array[z]=array[z+1];
+                }
+                i--;
+                array[i]='\0';
+                k--;
             }
         }
         else if(m==27)
@@ -117,37 +146,16 @@ void kui_input_box(int row,int column,char *array,int size,int screen_width,char
                 if(k<i)
                 {   
                     k++;
-                    if(k%screen_width==0)
-                    {
-                        cursor_row=cursor_row+1;
-                        cursor_column=column+1;
-                        kui_go_to_xy(cursor_row,cursor_column);
-                    }
-                    else
-                    {
-                        write(fileno(stdout),"\033[1C",4);
-                    }
                 }
-                cursorMoved=1;
+               // cursorMoved=1;
             }
             if(bb==91 && cc==68)
             {
                 // left arrow key
                 if(k>0 && i>0)
                 {
-                    if(k%screen_width==0)
-                    {
-                        cursor_row=cursor_row-1;
-                        cursor_column=column+screen_width;
-                        kui_go_to_xy(cursor_row,cursor_column);
-                    }
-                    else
-                    {
-                        write(fileno(stdout),"\033[1D",4);
-                    }
                     k--;
                 }
-                cursorMoved=1;
             }
         }
         else
@@ -169,10 +177,7 @@ void kui_input_box(int row,int column,char *array,int size,int screen_width,char
                 k++;
                 i++;
                 array[i]='\0';
-                cursor_row=row + (k/screen_width);
-                cursor_column=column + (k%screen_width)+1;
             }
-            cursorMoved=0;
         }
     }
     tcsetattr(fileno(stdout),0,&old_t);
@@ -189,6 +194,7 @@ int main()
                                          // 5th argument - screen width for input
                                          // 6th argument - left delimeter
                                          // 7th argument - right delimeter
-    printf("Text in box is : %s\n",a);
+    printf("Text in box is : (%s)\n",a);
+    printf("Length is : %ld\n",strlen(a));
     return 0;
 }
