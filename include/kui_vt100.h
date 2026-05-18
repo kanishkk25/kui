baa2c3b43d1a17770693ae67e86cb589e1c8768b
Kanishk <itskanishk36@gmail.com>
2026-05-19 00:33:26 +0530

    comments for doxygen

--- include/kui_vt100.h
+++ include/kui_vt100.h
@@ -6,15 +6,38 @@
 #ifndef __KUI_VT100_H
 #define __KUI_VT100_H
 
+/**
+ * @enum KUI_COLOR
+ *
+ * @brief Represent the colors on terminal
+ *
+ * Each enum value indicate the color used by VT100 renderer.
+ * 
+ */
 enum KUI_COLOR
 {
+    /** Black color indicator */
     KUI_COLOR_BLACK=101,
+
+    /** Red color indicator */
     KUI_COLOR_RED=102,
+
+    /** Green color indicator */
     KUI_COLOR_GREEN=103,
+
+    /** Yellow color indicator */
     KUI_COLOR_YELLOW=104,
+
+    /** Blue color indicator */
     KUI_COLOR_BLUE=105,
+
+    /** Magenta color indicator */
     KUI_COLOR_MAGENTA=106,
+
+    /** Cyan color indicator */
     KUI_COLOR_CYAN=107,
+
+    /** White color indicator */
     KUI_COLOR_WHITE=108
 };
 
@@ -24,12 +47,12 @@ enum KUI_COLOR
 void kui_clear();
 
 /**
- * @brief Clears the terminal screen
+ * @brief Clears the specific portion of the terminal screen
  *
- * @param row Row from which the contents should be cleared
- * @param column Colurm from which the contens should be cleared
- * @param height Height of the area whose contents should be cleared
- * @param widht Width of the area whose contents shpuld be cleared
+ * @param[in] row Row from which the portion should be cleared
+ * @param[in] column Column from which the portionshould be cleared
+ * @param[in] height Height of the portion that should be cleared
+ * @param[in] width Width of the the portion that should be cleared
  */
 void kui_clear_from_xy(int row,int column,int height,int width);
 
@@ -37,27 +60,27 @@ void kui_clear_from_xy(int row,int column,int height,int width);
 /**
  * @brief Moves the cursor to a specific screen position.
  *
- * @param row index of target row
- * @param column index of target column
+ * @param[in] row Index of target row
+ * @param[in] column Index of target column
  */
 void kui_go_to_xy(int row,int column);
 
 /**
  * @brief Prints text at a given position.
  *
- * @param row index of target row
- * @param column index of target column
- * @param str text to be print
+ * @param[in] row Index of target row
+ * @param[in] column Index of target column
+ * @param[in] str Text to be printed
  */
 void kui_say(int row,int column,char *str);
 
 
 /**
- * @brief Prints a char at a given position
+ * @brief Prints a character at a given position
  *
- * @param row index of target row
- * @param column index of target column
- * @param m char to be print
+ * @param[in] row Index of target row
+ * @param[in] column Index of target column
+ * @param[in] m Character to be printed
  */
 void kui_say_char(int row,int column,char m);
 
@@ -65,28 +88,28 @@ void kui_say_char(int row,int column,char m);
 /**
  * @brief Draws a horizontal line
  *
- * @param row index of row where the horizontal line should be drawn
- * @param column1 index of column from horizontal line should start
- * @param column2 index of column where horizontal line should end
+ * @param[in] row Index of row where the horizontal line should be drawn
+ * @param[in] column1 index of column from horizontal line should start
+ * @param[in] column2 index of column where horizontal line should end
  */
 void kui_draw_horizontal_line(int row,int column1,int column2);
 
 /**
  * @brief Draws a vertical line
  *
- * @param column index of column where the vertical line should be drawn
- * @param row1 index of row from where vertical line should start
- * @param row2 index of row where vertical line should end
+ * @param[in] column Index of column where the vertical line should be drawn
+ * @param[in] row1 Index of row from where vertical line should start
+ * @param[in] row2 Index of row where vertical line should end
  */
 void kui_draw_vertical_line(int column,int row1,int row2);
 
 /**
  * @brief Draws a box
  *
- * @param top_left_row Row of top-left corner
- * @param top_left_column Column of top-left corner
- * @param bottom_right_row Row of bottom-rigth corner
- * @param bottom_right_column Colmn of bottom-right corner
+ * @param[in] top_left_row Row of top-left corner
+ * @param[in] top_left_column Column of top-left corner
+ * @param[in] bottom_right_row Row of bottom-rigth corner
+ * @param[in] bottom_right_column Colmn of bottom-right corner
  */
 void kui_draw_box(int top_left_row,int top_left_column,int bottom_right_row,int bottom_right_column);
 
@@ -94,130 +117,120 @@ void kui_draw_box(int top_left_row,int top_left_column,int bottom_right_row,int 
 /**
  * @brief Draws a double horizontal line
  *
- * @param row index of row where the double horizontal line should be drawn
- * @param column1 index of column from double  horizontal line should start
- * @param column2 index of column where double horizontal line should end
+ * @param[in] row index of row where the double horizontal line should be drawn
+ * @param[in] column1 index of column from double  horizontal line should start
+ * @param[in] column2 index of column where double horizontal line should end
  */
 void kui_draw_double_horizontal_line(int row,int column1,int column2);
 
 /**
  * @brief Draws a double vertical line
  *
- * @param column index of column where the double vertical line should be drawn
- * @param row1 index of row from where double vertical line should start
- * @param row2 index of row where double vertical line should end
+ * @param[in] column index of column where the double vertical line should be drawn
+ * @param[in] row1 index of row from where double vertical line should start
+ * @param[in] row2 index of row where double vertical line should end
  */
 void kui_draw_double_vertical_line(int column,int row1,int row2);
 
 /**
  * @brief Draws a double line box
  *
- * @param top_left_row Row of top-left corner
- * @param top_left_column Column of top-left corner
- * @param bottom_right_row Row of bottom-rigth corner
- * @param bottom_right_column Colmn of bottom-right corner
+ * @param[in] top_left_row Row of top-left corner
+ * @param[in] top_left_column Column of top-left corner
+ * @param[in] bottom_right_row Row of bottom-rigth corner
+ * @param[in] bottom_right_column Colmn of bottom-right corner
  */
 void kui_draw_double_line_box(int top_left_row,int top_left_column,int bottom_right_row,int bottom_right_column);
 
-
-/*
- * Acceptable color list
- * - black
- * - red
- * - green
- * - yellow
- * - blue
- * - magenta
- * - cyan
- * - white
- *
- * NOTE: you have to pass one of these color name with as it is, because we are performing case-sensitive
- * string comparison under the hood.
- */
-
 /**
- * @brief Set background color
+ * @brief Sets the background color
  *
- * @param color background color
+ * @param[in] color Background color value from \ref KUI_COLOR
+ *
+ * @note The color remains active until another color or reset escape sequence is sent.
  */
 void kui_set_background_color(enum KUI_COLOR color);
 
 /**
- * @brief Set foreground color
+ * @brief Sets the foreground color
  *
- * @param color foreground color
- */
+ * @param[in] color Foreground color value from \ref KUI_COLOR
+ *
+ * @note The color remains active until another color or reset escape sequence is sent. */
 void kui_set_foreground_color(enum KUI_COLOR color);
 
 /**
  * @brief Set foreground and background color
  *
- * @param foregounrd_color foreground color
- * @param background_color background color
- */
+ * @param[in] foregounrd_color Foreground color value from \ref KUI_COLOR
+ * @param[in] background_color Background color value from \ref KUI_COLOR
+ *
+ * @note The color remains active until another color or reset escape sequence is sent. */
 void kui_set_color(enum KUI_COLOR foreground_color,enum KUI_COLOR background_color);
 
 /**
- * @brief Resets background color
- *
+ * @brief Remove the background color
  *
  */
 void kui_remove_background_color();
 
 /**
- * @brief Resets foreground color
+ * @brief Remove the foreground color
+ *
  */
 void kui_remove_foreground_color();
 
 /**
- * @brief Resets foreground and background color
+ * @brief Remove the foreground and background color
  */
 void kui_remove_color();
 
 /**
  * @brief Displays a fix-sized Menu list, allowing users to navigate additional options via scrolling
  * 
- * @param menu base address of 2D array containing options in menu
- * @param menu_size size of the menu list
- * @param visible_size default number of options to display 
- * @param row Row from where menu will be displayed
- * @param column Column from where menu will be displayed
- * @param height Height of the menu 
- * @param width Width of the menu
+ * @param[in] menu base address of 2D array containing options in menu
+ * @param[in] menu_size size of the menu list
+ * @param[in] visible_size default number of options to display 
+ * @param[in] row Row from where menu will be displayed
+ * @param[in] column Column from where menu will be displayed
+ * @param[in] height Height of the menu 
+ * @param[in] width Width of the menu
  *
  * @return The index of the selected option
- *
+ * 
+ * @note if some error occurend it will return -1
  */
 int kui_menu(char *menu[],int menu_size,int visible_size,int row,int column,int height,int width);
 
 /**
  * @brief Displays a fix-sized Choice list, allowing users to navigate using right / left arrow key
  *
- * @param choices Base address of 2D array containing options in choice
- * @param size Size of numbers of choices
- * @param row Row from where the choice list will be displayed
- * @param column Column from where the choice list will be displayed
+ * @param[in] choices Base address of 2D array containing options in choice
+ * @param[in] size Size of numbers of choices
+ * @param[in] row Row from where the choice list will be displayed
+ * @param[in] column Column from where the choice list will be displayed
  *
  * @return The index of the selected option
  *
+ * @note if some error occurend it will return -1
  */
 int kui_choice(char *choices[],int size,int row,int column);
 
 /**
  * @brief Displays a fix-sized Checkbox list, allowing users to navigate additional options via scrolling
  * 
- * @param menu base address of 2D array containing options in checkbox list
- * @param size size of the list
- * @param visible_size default number of options to display 
- * @param row Row from where menu will be displayed
- * @param column Column from where menu will be displayed
- * @param height Height of the menu 
- * @param width Width of the menu
- * @param result_size Base address of an int type variable that will store the number of items selected
+ * @param[in] menu base address of 2D array containing options in checkbox list
+ * @param[in] size size of the list
+ * @param[in] visible_size default number of options to display 
+ * @param[in] row Row from where menu will be displayed
+ * @param[in] column Column from where menu will be displayed
+ * @param[in] height Height of the menu 
+ * @param[in] width Width of the menu
+ * @param[out] result_size Base address of an int type variable where the number of selected items to be stored
  *
- * @return Base address of int type array which stores the list of selected indexes and
- * the user have to release the memroy of it when the work is done,(returns NULL if nothing selected)
+ * @return Base address of int type array where the list of selected indexes to be stored
  *
+ * @note The user have to release the memory of the retured array when the work is done (if item has been selected the function will return NULL)
  */
 int *  kui_checkbox(char *list[],int size, int visible_size,int row,int column,int height, int width,int *result_size);
 #endif
