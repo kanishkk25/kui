730148d07529e249f6b7d13d9f2ab546608d893e
Kanishk <itskanishk36@gmail.com>
2026-05-20 21:29:23 +0530

    fixed comments for doxygen

--- testcases/menu_testcase.c
+++ testcases/menu_testcase.c
@@ -1,11 +1,9 @@
 /**
 * @example menu_testcase.c
-* @author Kanishk Chouhan
-* @brief An example that uses KUI API to make Menu style UI on terminal
+*
+* An example of using \ref kui_menu function
 */
 
-/**
-@code
 #include<stdio.h>
 #include<termios.h>
 #include<stdlib.h>
@@ -13,12 +11,14 @@
 #include<kui_vt100.h>
 int main()
 {
-    char *menu[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve"};
+    char menu[]={"One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve"};
     
     int selected=kui_menu(menu,12,3,4,5,5,7);
     printf("\n");
     if(selected!=-1) printf("Selected option is : %s\n",menu[selected]);
     return 0;
 }
-@endcode
+
+/**
+* @author Kanishk Chouhan
 */
