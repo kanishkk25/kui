ed75e93b2810cf6eed8d029b89b785eb2f06b5f3
kanishk <itskanishk36@gmail.com>
2026-05-23 22:43:21 +0530

    testcases for kui_clear and kui_clear_from_xy

---
+++ testcases/kui_test1.c
@@ -0,0 +1,26 @@
+/**
+* @example kui_test1.c
+*
+* Example of using \ref kui_clear function
+*/
+
+#include<kui_vt100.h>
+#include<stdio.h>
+int main()
+{
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+    printf("This testcase demonstrate what kui_clear() does\n");
+
+    kui_clear();
+
+    return 0;
+}
---
+++ testcases/kui_test2.c
@@ -0,0 +1,21 @@
+#include<kui_vt100.h>
+#include<stdio.h>
+int main()
+{
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    printf("By using kui_clear_from_xy we can can clear specific portion of the screen\n");
+    
+    kui_clear_from_xy(4,7,10,20);
+
+    return 0;
+}
